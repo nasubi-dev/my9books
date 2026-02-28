26e80c363b1a766201dc13850c91e2e41714b5dd
nasubi-dev <r916nis@gmail.com>
2026-03-01 01:47:54 +0900

    future設定をコメントアウトし、SSRをデフォルトで有効にする設定を維持

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,7 +4,7 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-  future: {
-    v8_middleware: true,
-  },
+  // future: {
+  //   v8_middleware: true,
+  // },
 } satisfies Config
