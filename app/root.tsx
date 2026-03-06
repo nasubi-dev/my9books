12cca0d9d1e2b238aec7b13eb4ec47ba60dc675c
nasubi-dev <r916nis@gmail.com>
2026-03-06 16:58:35 +0900

    ClerkProviderのproxyUrlを絶対URLに更新

--- app/root.tsx
+++ app/root.tsx
@@ -73,7 +73,10 @@ export function Layout({ children }: { children: React.ReactNode }): JSX.Element
 
 export default function App({ loaderData }: Route.ComponentProps): JSX.Element {
   return (
-    <ClerkProvider loaderData={loaderData} proxyUrl="/clerk-proxy">
+    <ClerkProvider
+      loaderData={loaderData}
+      proxyUrl="https://my9books.nasubi.dev/clerk-proxy"
+    >
       <AppShell>
         <Outlet />
       </AppShell>
